d9d14b896ac5bffb7d8209fa6c5bcc32c414617f
Andersonjrx <juninho.anderson2303@gmail.com>
2025-11-23 23:10:41 -0300

    expandindo os dados para o resto do mundo

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 let cardContainer = document.querySelector(".card-container");
+let titulo = document.querySelector(".titulo");
 let campoBusca = document.querySelector("header input");
-let dados = [];
-iniciarBusca();
 
-async function iniciarBusca(){
-    if(dados.length === 0){
-        try {
-            let resposta = await fetch("data.json");
-            dados = await resposta.json();
-        } catch (erro) {
-            console.error("Falha ao buscar dados: ", erro);
-            return;
+let dadosBrasil = [];
+let dadosMundo = [];
+let dadosAtuais = [];
+
+carregarDados("Brasil");
+
+async function carregarDados(tipo) {
+    titulo.innerHTML = `WikiFut ${tipo}`;
+    campoBusca.value = "";
+    let body = document.body;
+
+    try {
+        if (tipo === "Brasil" && dadosBrasil.length === 0) {
+            const resposta = await fetch("data.json");
+            dadosBrasil = await resposta.json();
+            
+        } else if (tipo === "Mundo" && dadosMundo.length === 0) {
+            const resposta = await fetch("data2.json");
+            dadosMundo = await resposta.json();
+            body.classList.add('mundo-theme');
         }
+        
+        dadosAtuais = (tipo === "Brasil") ? dadosBrasil : dadosMundo;
+        tipo === "Brasil" ? body.classList.remove('mundo-theme') : body.classList.add('mundo-theme');
+        iniciarBusca();
+        
+    } catch (erro) {
+        console.error("Falha ao buscar dados: ", erro);
+        cardContainer.innerHTML = `<h2 class="sem-resultados">Falha ao carregar os dados. Tente novamente mais tarde.</h2>`;
     }
+}
 
+function iniciarBusca(){
     const termoBuscado = campoBusca.value.toLowerCase();
-    const dadosFiltrados = dados.filter(dado => 
+    const dadosFiltrados = dadosAtuais.filter(dado => 
         dado.nome.toLowerCase().includes(termoBuscado) ||
         dado.descricao.toLowerCase().includes(termoBuscado)
     );
-
     exibirCards(dadosFiltrados);
 }
 
@@ -50,3 +70,11 @@ campoBusca.addEventListener("keypress", function(event) {
         iniciarBusca();
     }
 });
+
+function cardsMundo(){
+    carregarDados("Mundo");
+}
+
+function cardsBrasil(){
+    carregarDados("Brasil");
+}
